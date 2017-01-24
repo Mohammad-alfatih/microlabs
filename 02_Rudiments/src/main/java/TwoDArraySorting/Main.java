ba65eb12bcea97f4fe6817977c7772d46516349c
Alfatih Mukhtar <alfatihmukhtar@zipcoders-MacBook-Pro-9.local>
2017-01-24 18:41:28 -0500

    Just for practice

---
+++ 02_Rudiments/src/main/java/TwoDArraySorting/Main.java
@@ -0,0 +1,9 @@
+package TwoDArraySorting;
+/**
+ * Created by alfatihmukhtar on 1/24/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 02_Rudiments/src/main/java/TwoDArraySorting/SortingBox.java
@@ -0,0 +1,16 @@
+package TwoDArraySorting;
+
+import java.util.Arrays;
+
+public class SortingBox {
+    int myArray[][] = new int[Math.random(int 10)][Math.random(int 10)];
+
+    for(int mumber[][] : myArray)
+
+    for (int number : myArray) {
+        System.out.print(number);
+    }
+
+    Arrays.sort(myArray);
+
+}
