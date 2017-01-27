f722f1bbf1e517b9e4382490efd730ef87b126ba
Alfatih Mukhtar <alfatihmukhtar@zipcoders-MacBook-Pro-9.local>
2017-01-27 10:58:47 -0500

    Added Level 4

---
+++ 02_Rudiments/Triangle/TriangleLevel3/TriangleLevel3.java
@@ -0,0 +1,12 @@
+class TriangleLevel3 {
+    public static void main(String[] args) {
+        // Same exact code as TriangleLevel2 App. Because this code still meets the conditons.
+        for(int j=5;j>0;j--) {
+            for(int i=j;i>0;i--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        
+    }
+}
---
+++ 02_Rudiments/Triangle/TriangleLevel4/TriangleLevel4.java
@@ -0,0 +1,17 @@
+class TriangleLevel4 {
+    public static void main(String[] args) {
+        
+        int j = 5;
+        while(j>0) {
+            
+            int i = j;
+            while(i>0) {
+                System.out.print("*");
+                i--;
+            }
+            
+            System.out.print("\n");
+            j--;
+        }
+    }
+}
